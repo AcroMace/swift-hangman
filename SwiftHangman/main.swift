a1d60ed6318130c3ad6a39e6dbe2fda769d36958
Andy <acromace@gmail.com>
2014-06-07 21:57:36 -0400

    Initial Commit

---
+++ SwiftHangman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHangman
+//
+//  Created by Andy Cho on 2014-06-07.
+//  Copyright (c) 2014 Andy Cho. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
